624b7c8dba8bc2bd7c920ee63dfb26463415c017
AleksBeliun <aleksandr@nvoresume.com>
2023-12-06 16:06:38 +0100

    feat: :sparkles: Add form validation to voyage creation API

--- src/pages/api/voyage/add.ts
+++ src/pages/api/voyage/add.ts
@@ -1,12 +1,18 @@
 import type { NextApiHandler, NextApiResponse, NextApiRequest } from "next";
 import { createVoyage } from "~/server/voyage";
+import { formSchema } from "~/components/form/createVoyage";
 
 const handler: NextApiHandler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
   if (req.method === "POST") {
-    const newVoyage = await createVoyage(req.body);
+    const parsedBody = formSchema.safeParse(req.body);
+    if (!parsedBody.success) {
+      res.status(400).json(parsedBody.error);
+      return;
+    }
+    const newVoyage = await createVoyage(parsedBody.data);
 
     newVoyage ? res.status(201) : res.status(404);
     res.end();
