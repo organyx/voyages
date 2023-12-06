6c2e2e8a57457fbe039b04385a11966cc3b82ceb
AleksBeliun <aleksandr@nvoresume.com>
2023-12-06 11:41:57 +0100

    test: :white_check_mark: Add unitType getAll test

---
+++ src/pages/api/unitType/getAll.test.ts
@@ -0,0 +1,19 @@
+import { createMocks } from "node-mocks-http";
+import getAll from "./getAll";
+import * as unitType from "../../../server/unitType";
+
+describe("GET /api/unitType/getAll", () => {
+  it("should return 200 OK", async () => {
+    vi.spyOn(unitType, "getAllUnitTypes").mockImplementation(() => {
+      return Promise.resolve([]);
+    });
+    const { req, res } = createMocks({
+      method: "GET",
+    });
+
+    await getAll(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+    expect(JSON.parse(res._getData())).toEqual([]);
+  });
+});
