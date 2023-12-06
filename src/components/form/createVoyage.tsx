89296cabc6f7f9d0aa8cb4471e76005493f9c794
AleksBeliun <aleksandr@nvoresume.com>
2023-12-06 15:11:11 +0100

    feat: :sparkles: Add default values for scheduled departure and arrival

--- src/components/form/createVoyage.tsx
+++ src/components/form/createVoyage.tsx
@@ -38,6 +38,8 @@ export function VoyageForm({ setIsSheetOpen }: VoyageFormProps) {
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      scheduledDeparture: '',
+      scheduledArrival: '',
       portOfLoading: '',
       portOfDischarge: '',
       vesselId: '',
