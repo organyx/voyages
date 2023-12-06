e8da558dab150b181681db09660555884062d47e
AleksBeliun <aleksandr@nvoresume.com>
2023-12-06 11:44:41 +0100

    test: :white_check_mark: Add unit test for getAll API endpoint

---
+++ src/pages/api/vessel/getAll.test.ts
@@ -0,0 +1,19 @@
+import { createMocks } from "node-mocks-http";
+import getAll from "./getAll";
+import * as vessel from "~/server/vessel";
+
+describe("GET /api/vessel/getAll", () => {
+  it("should return 200 OK", async () => {
+    vi.spyOn(vessel, "getAllVessels").mockImplementation(() => {
+      return Promise.resolve([]);
+    });
+    const { req, res } = createMocks({
+      method: "GET",
+    });
+
+    await getAll(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+    expect(JSON.parse(res._getData())).toEqual([]);
+  });
+});
